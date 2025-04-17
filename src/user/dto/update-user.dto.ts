f67d120d71cdb5663e98e1b76cbd0204b12f17be
麦耀强 <maiyaoqiang@zhongshitech.cn>
2025-04-17 11:43:30 +0800

    feat: 添加passwordexclude

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -2,28 +2,26 @@ import { IsOptional, IsString, IsBoolean, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 
-@Exclude()
 export class UpdateUserDto {
-  @Expose()
   @ApiProperty({ description: '用户ID', required: true })
   @IsNumber()
   id: number;
 
-  @Expose()
   @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   phone?: string;
 
-  @Expose()
   @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   role?: string;
 
-  @Expose()
   @ApiProperty({ required: false })
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
+
+  @Exclude()
+  password?: string;
 }
